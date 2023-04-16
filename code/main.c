0a7a5aaa3dc8ceab83951726fd8c3205979484e0
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2023-04-16 16:43:16 +0200

    MACRO to static inline, cache_size, mem bomb init.
    Cache size from ccsidr_el1 after set csselr_el1 to 0x2 for L2 cache info. Switch case since ARM Cortex-A72 L2 cache can be 512 / 1024 / 2048 or 4096 KB.

--- code/main.c
+++ code/main.c
@@ -18,54 +18,57 @@ typedef u32         uint32_t;
 
 #define ARMV8_PMCNTENSET_EL0_ENABLE (1<<31) /* *< Enable Perf count reg */
 
-#define SYSREG_NUM(reg) \
-    ({ unsigned long long __val; \
-        __asm__ volatile ("mrs %0, " #reg : "=r" (__val)); \
-        __val; })
-
-#define SYSREG(reg) (1ULL << SYSREG_NUM(reg))
-
-unsigned long long read_sysreg(const char* reg_name)
-{
-	unsigned long long reg_val;
-
-	#define REG_MAP(reg) else if (!strcmp(reg_name, #reg)) { \
-		__asm__ volatile ("mrs %0, " #reg : "=r" (reg_val)); }
-	if (0) {}
-	// sys 14
-	REG_MAP(pmevcntr0_el0)
-	REG_MAP(pmevcntr1_el0)
-	REG_MAP(pmevcntr2_el0)
-	REG_MAP(pmevcntr3_el0)
-	REG_MAP(pmevcntr4_el0)
-	REG_MAP(pmevcntr5_el0)
-	REG_MAP(pmevtyper0_el0)
-	REG_MAP(pmevtyper1_el0)
-	REG_MAP(pmevtyper2_el0)
-	REG_MAP(pmevtyper3_el0)
-	REG_MAP(pmevtyper4_el0)
-	REG_MAP(pmevtyper5_el0)
-
-	// sys 9
-	REG_MAP(pmcr_el0)
-	REG_MAP(pmccntr_el0)
-	REG_MAP(pmxevtyper_el0)
-	REG_MAP(pmuserenr_el0)
-	REG_MAP(pmxevcntr_el0)
-
-	else {
-		print("Register name not mapped\n");
-		exit(0);
-	    return 0;
-	}
-	return reg_val;
-}
-
+// Performance Monitors Control Register
 static inline u32 armv8pmu_pmcr_read(void)
 {
 	u64 val=0;
 	asm volatile("mrs %0, pmcr_el0" : "=r" (val));
 	return (u32)val;
+}
+
+// Performance Monitors Cycle Count Register
+static inline u32 armv8pmu_pmccntr_read(void)
+{
+	u64 val=0;
+	asm volatile("mrs %0, pmccntr_el0" : "=r" (val));
+	return (u32)val;
+}
+
+// Set Cache Level
+static inline void armv8pmu_set_cache_level() {
+    u64 val = 0x2;
+
+    __asm__ volatile ("msr csselr_el1, %0" : : "r" (val));
+}
+
+// Read Cache Size ID Register
+static inline u32 armv8pmu_read_cache_size() {
+    u64 val=0;
+
+    __asm__ volatile ("mrs %0, ccsidr_el1": "=r" (val));
+    switch(val) {
+    	case 0x701FE00A:
+    		printf("32 KB");
+    		break;
+    	case 0x201FE012:
+    		printf("48 KB");
+    	    break;
+    	case 0x703FE07A:
+    		printf("512 KB");
+    	    break;
+    	case 0x707FE07A:
+    	    printf("1024 KB");
+    	    break;
+    	case 0x70FFE07A:
+    		printf("2048 KB");
+    	    break;
+    	case 0x71FFE07A:
+    		printf("4096 KB");
+    	    break;
+    	default:
+    	    printf("Unknown value: 0x%lx\n", val);
+    }
+    return (u32)val;
 }
 
 static inline void armv8pmu_pmcr_write(u32 val)
@@ -83,28 +86,44 @@ static inline void reset_pmuserenr_el0()
 int main()
 {
     init_platform();
-    unsigned long long reg_val;
-    unsigned long long event_number = 0x13; //Memory access
 
     /* Reset pmuserenr_el0 to default */
     reset_pmuserenr_el0();
+
     /*  Enable user-mode access to counters. */
     __asm__ volatile("msr pmuserenr_el0, %0" :: "r"((u64)ARMV8_PMUSERENR_EN_EL0|ARMV8_PMUSERENR_ER|ARMV8_PMUSERENR_CR));
+
     /*  Initialize & Reset PMNC: C and P bits. */
     armv8pmu_pmcr_write(ARMV8_PMCR_P | ARMV8_PMCR_C);
 
     /* Set event type to event_number in pmxevtyper_el0 register. */
+    unsigned long long event_number = 0x17; //Level 2 data cache refill (miss)
     __asm__ volatile ("msr pmxevtyper_el0, %0" :: "r" (event_number));
+
     /*   Performance Monitors Count Enable Set register bit 30:0 disable, 31 enable */
     __asm__ volatile("msr pmcntenset_el0, %0" :: "r" (ARMV8_PMCNTENSET_EL0_ENABLE));
+
+    /* Set cache level */
+    armv8pmu_set_cache_level(2);
+
+    /* Print Cache Size (L2) */
+    armv8pmu_read_cache_size();
+
     /* start*/
     armv8pmu_pmcr_write(armv8pmu_pmcr_read() | ARMV8_PMCR_E);
 
+    int i;
+    int n = 512 * 512;
+    int *block = malloc(n * sizeof(int)); // (n * n * 8) > 1024KB (L2 size)
 
+    for (i = 0; i < n / 10; i++) {
+    	int ri = rand() % n;
+        block[ri] = 0;
+    }
 
-    /* Read value from a specific register mapped to a macro definition */
-    reg_val = read_sysreg("pmccntr_el0");
-    printf("CPU Cycles: %llu\n", reg_val);
+    u32 reg_val = armv8pmu_pmccntr_read();
+    printf("\nRegister value: %u\n", reg_val);
+
 
     cleanup_platform();
     return 0;
