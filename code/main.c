8e41f3c721d56806275833b4f95b96856a1a3b5d
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2023-06-03 16:05:39 +0200

    pthread and nice
    - pthread with 2 thread for Cortex-A72
    - nice(-20) for process priority
    - clock_gettime support

--- code/main.c
+++ code/main.c
@@ -2,199 +2,166 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
-#include "platform.h"
-#include "xil_printf.h"
-#include "pmuv3.h"
+#include <unistd.h>
+#include <pthread.h>
 
-typedef u64 uint64_t;
-typedef u32 uint32_t;
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint8_t u8;
 
-#define NUM_ACCESSES 1000
+#define NUM_ACCESSES 10000
 #define L2_CACHE_SIZE 0x100000 // 1024KB => 1MB
 #define CACHE_LINE_SIZE 64     // 64B
 
-u32 read_cache_misses_sequential(volatile u32 *memory, size_t size)
-{
-	u32 start, end;
-	size_t i;
 
-    if (NUM_ACCESSES==1)
-    {
-    	start = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
-    	for (i = 0; i < NUM_ACCESSES; i += CACHE_LINE_SIZE)
+struct timespec start_time, end_time;
+
+
+void read_cache_misses_sequential(volatile u8 *memory, u32 size)
+{
+    int i;
+
+    	for (i = 0; i < size; i += CACHE_LINE_SIZE)
     	{
-    		u32 value = memory[i];
-    	    (void)value;
+    		u8 value = memory[i];
+    	    	(void)value;
     	}
-    	end = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
-    } else {
-    	start = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
-    	    	for (i = 0; i < size; i += CACHE_LINE_SIZE)
-    	    	{
-    	    		u32 value = memory[i];
-    	    	    (void)value;
-    	    	}
-    	end = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
-    }
-    return (end - start);
 }
 
-u64 read_cache_misses_random(volatile u32 *memory, size_t size)
-{
-    u64 start, end;
-    size_t i;
 
-    // Start measuring performance counter
-    start = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
+void read_cache_misses_random(volatile u8 *memory, u32 size)
+{
+    int i;
 
     // Random read
-    for (i = 0; i < NUM_ACCESSES; i++)
+    for (i = 0; i < size; i+= CACHE_LINE_SIZE)
     {
-        size_t index = rand() % size;
-        u32 value = memory[index];
+        int index = rand() % size;
+        u8 value = memory[index];
         (void)value;
     }
-    // Stop measuring performance counter
-    end = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
-
-    return (end - start);
 }
 
-u64 write_cache_misses_sequential(volatile u32 *memory, size_t size)
-{
-    u64 start, end;
-    size_t i;
 
-    start = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
-    for (i = 0; i < NUM_ACCESSES; i++)
+void write_cache_misses_sequential(volatile u8 *memory, u32 size)
+{
+    int i;
+
+    for (i = 0; i < size; i += CACHE_LINE_SIZE)
     {
-        memory[i] = i;
+        memory[i] = 0;
     }
-    end = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
-
-    return (end - start);
 }
 
-u64 write_cache_misses_random(volatile u32 *memory, size_t size)
+
+void write_cache_misses_random(volatile u8 *memory, u32 size)
 {
-    u64 start, end;
-    size_t i;
-    unsigned long long sum = 0;
+    int i;
 
     // Random access pattern
-    start = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
-    for (i = 0; i < NUM_ACCESSES; i++)
+    for (i = 0; i < size; i += CACHE_LINE_SIZE)
     {
         int rand_index = rand() % size;
-        memory[rand_index * CACHE_LINE_SIZE] = 1;
-        sum += memory[rand_index * CACHE_LINE_SIZE];
+        memory[rand_index] = 0;
     }
-    end = armv8pmu_read_counter(ARMV8_IDX_COUNTER1);
-
-    return (end - start);
 }
 
-// Read Cache Size ID Register
-static inline u32 armv8pmu_read_cache_size()
-{
-    u64 val = 0;
 
-    __asm__ volatile("mrs %0, ccsidr_el1"
-                     : "=r"(val));
-    switch (val)
-    {
-    case 0x701FE00A:
-        printf("Cache size: 32 KB\n\n");
-        break;
-    case 0x201FE012:
-        printf("Cache size: 48 KB\n\n");
-        break;
-    case 0x703FE07A:
-        printf("Cache size: 512 KB\n\n");
-        break;
-    case 0x707FE07A:
-        printf("Cache size: 1024 KB\n\n");
-        break;
-    case 0x70FFE07A:
-        printf("Cache size: 2048 KB\n\n");
-        break;
-    case 0x71FFE07A:
-        printf("Cache size: 4096 KB\n\n");
-        break;
-    default:
-        printf("Unknown value: 0x%lx\n", val);
-    }
-    return (u32)val;
-}
+void* thread_function() {
+ 
+    volatile u8 *memory;
+    u32 size = L2_CACHE_SIZE;
+    int m_size = L2_CACHE_SIZE * 2;
+    double elapsed_time;
 
-int main()
-{
-	init_platform();
 
-	u32 start, end;
-
-    // Initialize random seed
-    srand(12345);
-
-    armv8pmu_init_counter(ARMV8_IDX_COUNTER1, L2D_CACHE_REFILL);
-    armv8pmu_init_counter(ARMV8_IDX_COUNTER2, MEM_ACCESS);
-    armv8pmu_init_counter(ARMV8_IDX_COUNTER3, L2D_CACHE_WB);
-    armv8pmu_start();
-
-    volatile u32 *memory;
-    size_t size = NUM_ACCESSES * CACHE_LINE_SIZE;
-    int m_size = L2_CACHE_SIZE * sizeof(int);
+    // Allocate a memory block that is larger than the L2 cache size
     memory = malloc(m_size);
 
     // Measure cache misses for sequential read
-    start = armv8pmu_read_counter(ARMV8_IDX_COUNTER2);
-    u32 read_misses_seq = read_cache_misses_sequential(memory, size);
-    end = armv8pmu_read_counter(ARMV8_IDX_COUNTER2);
-    printf("\nSequential read cache misses: %d\n", read_misses_seq);
-    printf("Memory Access: %d\n", (end - start));
+    clock_gettime(CLOCK_MONOTONIC, &start_time); // start clock
+    for (int i = 0; i < NUM_ACCESSES; i++) {
+    	read_cache_misses_sequential(memory, size);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end_time); // end clock
+    printf("\nSequential Read\n\nMalloc size: %d\nNum Access: %d\nElapsed time: %.6f seconds\n\n", m_size, NUM_ACCESSES, (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_nsec - start_time.tv_nsec) / 1e9);
 
     // Free the memory block
     free((void *)memory);
 
+
+    // Allocate a memory block that is larger than the L2 cache size
     memory = malloc(m_size);
 
     // Measure cache misses for random read
-    start = armv8pmu_read_counter(ARMV8_IDX_COUNTER2);
-    u32 read_misses_rand = read_cache_misses_random(memory, size);
-    end = armv8pmu_read_counter(ARMV8_IDX_COUNTER2);
-    printf("\nRandom read cache misses: %d\n", read_misses_rand);
-    printf("Memory Access: %d\n", (end - start));
+    clock_gettime(CLOCK_MONOTONIC, &start_time); // start clock
+    for (int i = 0; i < NUM_ACCESSES; i++) {
+    	read_cache_misses_random(memory, size);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end_time); // end clock
+    printf("\nRandom Read\n\nMalloc size: %d\nNum Access: %d\nElapsed time: %.6f seconds\n\n", m_size, NUM_ACCESSES, (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_nsec - start_time.tv_nsec) / 1e9);
 
     // Free the memory block
     free((void *)memory);
+
 
     // Allocate a memory block that is larger than the L2 cache size
     memory = malloc(m_size);
 
     // Measure cache misses for sequential write
-    start = armv8pmu_read_counter(ARMV8_IDX_COUNTER3);
-    u32 write_misses_seq = write_cache_misses_sequential(memory, size);
-    end = armv8pmu_read_counter(ARMV8_IDX_COUNTER3);
-    printf("\nSequential write cache misses: %d\n", write_misses_seq);
-    printf("L2D_CACHE_WB: %d\n", (end - start));
+    clock_gettime(CLOCK_MONOTONIC, &start_time); // start clock
+    for (int i = 0; i < NUM_ACCESSES; i++) {
+    	write_cache_misses_sequential(memory, size);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end_time); // end clock
+    printf("\nSequential Write\n\nMalloc size: %d\nNum Access: %d\nElapsed time: %.6f seconds\n\n", m_size, NUM_ACCESSES, (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_nsec - start_time.tv_nsec) / 1e9);
 
     // Free the memory block
     free((void *)memory);
 
+
+    
+    // Allocate a memory block that is larger than the L2 cache size
     memory = malloc(m_size);
 
     // Measure cache misses for random write
-    start = armv8pmu_read_counter(ARMV8_IDX_COUNTER2);
-    u32 write_misses_rand = write_cache_misses_random(memory, size);
-    end = armv8pmu_read_counter(ARMV8_IDX_COUNTER2);
-    printf("\nRandom write cache misses: %d\n", write_misses_rand);
-    printf("Memory Access: %d\n", (end - start));
+    clock_gettime(CLOCK_MONOTONIC, &start_time); // start clock
+    for (int i = 0; i < NUM_ACCESSES; i++) {
+    	write_cache_misses_random(memory, size);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end_time); // end clock
+    printf("\nRandom Write\n\nMalloc size: %d\nNum Access: %d\nElapsed time: %.6f seconds\n\n", m_size, NUM_ACCESSES, (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_nsec - start_time.tv_nsec) / 1e9);
 
     // Free the memory block
     free((void *)memory);
+}
 
-    armv8pmu_stop();
 
-    cleanup_platform();
+int main()
+{
+    // process priority value
+    int niceValue = -20;
+
+    // set process priority
+    nice(niceValue);
+    
+    // Initialize random seed
+    srand(1234);
+
+    // Cortex-A72 Dual Core => 2 thread
+    pthread_t thread1, thread2;  // thread identifiers
+    
+    // Create thread 1
+    pthread_create(&thread1, NULL, thread_function, NULL);
+
+    // Create thread 2
+    pthread_create(&thread2, NULL, thread_function, NULL);
+
+    // Wait for thread 1 to finish
+    pthread_join(thread1, NULL);
+
+    // Wait for thread 2 to finish
+    pthread_join(thread2, NULL);
+	
     return 0;
 }
